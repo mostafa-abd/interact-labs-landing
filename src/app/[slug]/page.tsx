236be4e67017138dfc48cffa11f7a611ee17b2d7
mostafa <mostafa-for-work@hotmail.com>
2025-11-03 00:16:28 +0200

    Update page.tsx

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,30 @@
+// app/[slug]/page.tsx
+"use client";
+// import React from "react";
+
+// import products from "@/data/products.json";
+// import MainSection from "../components/main-section";
+// import Benefits from "../components/benefits";
+// import Feedback from "../components/feedback";
+// import ProductDetails from "../components/product-details";
+// import "../assets/css/product.css";
+// interface Props {
+//   params: Promise<{ slug: string }>;
+// }
+
+export default function ProductPage() {
+  // // ✅ unwrap the params using React.use
+  // const { slug } = React.use(params);
+
+  // const product = products[slug as keyof typeof products];
+  // if (!product) return <h1>Product not found</h1>;
+
+  return (
+    <main>
+      {/* <MainSection product={product} />
+      <Benefits />
+      <Feedback />
+      <ProductDetails product={product} /> */}
+    </main>
+  );
+}
