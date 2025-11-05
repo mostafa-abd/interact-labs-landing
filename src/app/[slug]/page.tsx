cb98081c64ef2726a05aa9898a3af7055eb7e8c2
mostafa <mostafa-for-work@hotmail.com>
2025-11-05 05:18:40 +0200

    Update page.tsx

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,6 +1,4 @@
-"use client";
 import React from "react";
-
 import products from "@/data/products.json";
 import MainSection from "../components/main-section";
 import Benefits from "../components/benefits";
@@ -9,14 +7,14 @@ import ProductDetails from "../components/product-details";
 import "../assets/css/product.css";
 
 interface Props {
-  params: { slug: string };
+  params: { slug?: string };
 }
+
 export const runtime = "edge";
 
 export default function ProductPage({ params }: Props) {
-  const { slug } = params;
-
-  const normalizedSlug = slug.toLowerCase(); // 👈 هنا التحويل
+  const slug = params?.slug || ""; // 👈 تجنب undefined
+  const normalizedSlug = slug.toLowerCase(); // 👈 بعد التأكد إنها string
   const product = products[normalizedSlug as keyof typeof products];
 
   if (!product) return <h1>Product not found</h1>;
