a1df1e550ed988776a5e4a20a5c26a695e55a95b
mostafa <mostafa-for-work@hotmail.com>
2025-11-06 03:56:32 +0200

    Update route.ts

--- src/app/api/paymob/response/route.ts
+++ src/app/api/paymob/response/route.ts
@@ -6,12 +6,11 @@ export async function GET(req: Request) {
     const url = new URL(req.url);
     const success = url.searchParams.get("success");
 
-if (success === "true") {
-  return NextResponse.redirect("/thanks");
-} else {
-  return NextResponse.redirect("/Failure");
-}
-
+    if (success === "true") {
+      return NextResponse.redirect(`${process.env.NEXT_PUBLIC_URL}/thanks`);
+    } else {
+      return NextResponse.redirect(`${process.env.NEXT_PUBLIC_URL}/Failure`);
+    }
   } catch (error) {
     console.error("❌ Error in payment response:", error);
     return NextResponse.json(
