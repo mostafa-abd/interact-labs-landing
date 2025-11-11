de8dc3e5e687e2c20ed19631f938484b49590d57
Mostafa AbdElmogod <155806017+mostafa-abd@users.noreply.github.com>
2025-11-11 13:17:26 +0200

    Update page title and add Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import Header from "./includes/header";
 import Footer from "./includes/footer";
 import { LanguageProvider } from "./context/LanguageContext";
 
 export const metadata: Metadata = {
-  title: "Shop Interact",
+  title: "Interact Shop",
   icons: {
     icon: "/favicon.ico",
   },
@@ -17,14 +18,39 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html suppressHydrationWarning>
-      <LanguageProvider>
-        <body className="antialiased">
+    <html suppressHydrationWarning lang="en">
+      {/* Google Tag Manager (in head) */}
+      <Script
+        id="google-tag-manager"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5R7FJH6B');
+          `,
+        }}
+      />
+
+      <body className="antialiased">
+        {/* Google Tag Manager (noscript in body) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5R7FJH6B"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
+        <LanguageProvider>
           <Header />
           {children}
           <Footer />
-        </body>
-      </LanguageProvider>
+        </LanguageProvider>
+      </body>
     </html>
   );
 }
