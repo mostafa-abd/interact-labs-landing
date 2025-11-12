9436f741a43e71271a85f19da33dbbb88a9f2320
mostafa <mostafa-for-work@hotmail.com>
2025-11-13 00:33:40 +0200

    Update page.tsx

--- src/app/checkout/[slug]/page.tsx
+++ src/app/checkout/[slug]/page.tsx
@@ -1,15 +1,13 @@
+export const runtime = "edge";
+
 import CheckoutPage from "./CheckoutPage";
 import type { Metadata } from "next";
-
-
-
 export function generateMetadata(): Metadata {
   return {
     title: `Checkout `,
     description: `Complete your order safely and easily.`,
   };
 }
-
 export default function Page() {
   return <CheckoutPage />;
 }
